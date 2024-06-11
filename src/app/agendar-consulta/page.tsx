9b673222577e61b5608e104b50e82d918b23b676
Valéria Pessoa <valeriapessoa2005@gmail.com>
2024-06-10 23:27:02 -0300

    atualização

--- src/app/agendar-consulta/page.tsx
+++ src/app/agendar-consulta/page.tsx
@@ -2,6 +2,7 @@
 
 import Head from 'next/head';
 import styles from '../styles/agendar-consulta.module.css';
+import { FaBullseye } from 'react-icons/fa';
 
 const AgendarConsulta = () => {
   return (
@@ -10,39 +11,38 @@ const AgendarConsulta = () => {
         <title>Agendar Consulta</title>
       </Head>
       <img src="/images/agendar-consulta/img-1.png" className={styles.banner} />
-      <div className={styles.div_destaque}></div>
-      <div className={styles.div_agendamento}>
-        <h1 className={styles.texto}>Agende sua consulta</h1>
+      <div className={styles.highlight}></div>
+      <div className={styles.scheduling}>
+        <h1 className={styles.title}>Agende sua consulta</h1>
       </div>
-      <section className={`container ${styles.container}`}>
-        <div className={`card ${styles.card}`}>
+      <section className={styles.container}>
+        <div className={styles.card}>
           <div>
-            <img className={styles.icones} src="/images/agendar-consulta/img-2.png" />
-            <p className={styles.texto1}>Boa localização</p>
+            <img className={styles.icons} src="/images/agendar-consulta/img-2.png" />
+            <p className={styles.text}>Boa localização</p>
           </div>
         </div>
-        <div className={`card ${styles.card}`}>
+        <div className={styles.card}>
           <div>
-            <img className={styles.icones} src="/images/agendar-consulta/img-3.png" />
-            <p className={styles.texto1}>Escolha o melhor dia e horário</p>
+            <img className={styles.icons} src="/images/agendar-consulta/img-3.png" />
+            <p className={styles.text}>Escolha o melhor dia e horário</p>
           </div>
         </div>
-        <div className={`card ${styles.card}`}>
+        <div className={styles.card}>
           <div>
-            <img className={styles.icones} src="/images/agendar-consulta/img-4.png" />
-            <p className={styles.texto1}>Prático e seguro</p>
+            <img className={styles.icons} src="/images/agendar-consulta/img-4.png" />
+            <p className={styles.text}>Prático e seguro</p>
           </div>
         </div>
       </section>
-      <div className={styles.div_Agendamento}>
-        <a href="login.php" target="_blank" className={styles.botao_azul}>
-          <span className="material-icons">ads_click</span><span> Agendamento Online</span>
-        </a>
-      </div>
+      <div className={styles.onlineScheduling}>
+      <a href="login.php" target="_blank" className={styles.blueButton}>
+        <FaBullseye className={styles.icon} />
+        <span> Agendamento Online</span>
+      </a>
+    </div>
     </main>
   );
 };
 
 export default AgendarConsulta;
-
-
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { FaPhoneAlt } from 'react-icons/fa';
 
+// Componente do cabeçalho
 const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className="custom-navbar">
@@ -13,16 +14,16 @@ const Header = () => {
           <img
             src="./logo-white.png"
             alt="e-Clinic Logo"
-            className="logo"
+            className="header-logo"
           />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className='navbar-toggler' />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/convenios">Convênios</Nav.Link>
-            <Nav.Link href="/unidades">Unidades</Nav.Link>
-            <Nav.Link href="/agendar-consulta">Agendar Consulta</Nav.Link>
+          <Nav className="header-nav">
+            <Nav.Link href="/" active={window.location.pathname === '/'}>Home</Nav.Link>
+            <Nav.Link href="/convenios" active={window.location.pathname === '/convenios'}>Serviços</Nav.Link>
+            <Nav.Link href="/unidades" active={window.location.pathname === '/unidades'}>Unidades</Nav.Link>
+            <Nav.Link href="/agendar-consulta" active={window.location.pathname === '/agendar-consulta'}>Agendar Consulta</Nav.Link>
           </Nav>
           <Nav className='header-phone'>
             <Nav.Link href="tel:1122403434">
