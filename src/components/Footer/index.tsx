98cc65a665cc90eed1fc79cf73ee6183108aa1bd
Valéria Pessoa <valeriapessoa2005@gmail.com>
2024-06-13 18:12:54 -0300

    Alterei os nomes das classes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,10 +9,10 @@ const Footer = () => {
   const isActive = (path: string) => pathname === path ? 'link active': 'link';
   return (
     <footer className="footer">
-      <section className="container-flex">
-        <div className="recipiente">
+      <section className="flex-container">
+        <div className="wrapper">
           <div className="footer-item">
-            <a href="/"><img src="/logo-white.png" className="logo_branco" alt="Logo E-Clinic" /></a>
+            <a href="/"><img src="/logo-white.png" className="white-logo" alt="Logo E-Clinic" /></a>
           </div>
           <div className="footer-item">
             <p><a className={isActive('/')} href="/">Home</a></p>
@@ -22,20 +22,20 @@ const Footer = () => {
           </div>
           <div className="footer-item">
             <p>Entre em contato conosco:</p>
-            <div className="footer-contato">
-              <FaPhoneAlt className="contato-icone" />
-              <a className="footer-telefone" href="tel:11-3244-2020">(11) 34724-1234</a>
+            <div className="footer-contact">
+              <FaPhoneAlt className="contact-icon" />
+              <a className="footer-phone" href="tel:11-3244-2020">(11) 34724-1234</a>
             </div>
           </div>
-          <div className="footer-item footer-item--social">
+          <div className="footer-item social-links">
             <p>Nos acompanhe nas redes sociais:</p>
-            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare className="redes-sociais" /></a>
-            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebookSquare className="redes-sociais" /></a>
-            <a href="https://twitter.com/?lang=pt-br" target="_blank" rel="noopener noreferrer"><FaTwitterSquare className="redes-sociais" /></a>
+            <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><FaInstagramSquare className="social-icon" /></a>
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FaFacebookSquare className="social-icon" /></a>
+            <a href="https://twitter.com/?lang=pt-br" target="_blank" rel="noopener noreferrer"><FaTwitterSquare className="social-icon" /></a>
           </div>
         </div>
       </section>
-      <div className="footer-termo">
+      <div className="footer-disclaimer">
         ©2024 - E-Clinic - Todos os direitos reservados - Termos de Uso e Política de Privacidade
       </div>
     </footer>
