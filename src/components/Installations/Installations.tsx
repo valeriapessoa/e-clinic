ad63313275e069b9ab200ebf7a17598f96d768bf
Valéria Pessoa <valeriapessoa2005@gmail.com>
2024-12-27 20:00:05 -0300

    Atualização no layout para celulares e tablets - Home/Installations

--- src/components/Installations/Installations.tsx
+++ src/components/Installations/Installations.tsx
@@ -28,14 +28,14 @@ const Installations: React.FC = () => {
   // Lista de imagens
   const items = [
     { src: "/images/home/img-1.jpg", alt: "Instalação 1" },
-    { src: "/images/home/img-2.jpg", alt: "Instalação 2" },
-    { src: "/images/home/img-3.jpg", alt: "Instalação 3" },
-    { src: "/images/home/img-4.jpg", alt: "Instalação 4" },
-    { src: "/images/home/img-5.jpg", alt: "Instalação 5" },
-    { src: "/images/home/img-6.jpg", alt: "Instalação 6" },
-    { src: "/images/home/img-7.jpg", alt: "Instalação 7" },
-    { src: "/images/home/img-8.jpg", alt: "Instalação 8" },
-    { src: "/images/home/img-9.jpg", alt: "Instalação 9" },
+    { src: "/images/home/img-2.png", alt: "Instalação 2" },
+    { src: "/images/home/img-3.png", alt: "Instalação 3" },
+    { src: "/images/home/img-4.png", alt: "Instalação 4" },
+    { src: "/images/home/img-5.png", alt: "Instalação 5" },
+    { src: "/images/home/img-6.png", alt: "Instalação 6" },
+    { src: "/images/home/img-7.png", alt: "Instalação 7" },
+    { src: "/images/home/img-8.png", alt: "Instalação 8" },
+    { src: "/images/home/img-9.png", alt: "Instalação 9" },
   ];
 
   // Calcular o número total de slides
@@ -52,7 +52,8 @@ const Installations: React.FC = () => {
         />
         <p className={`mb-4 ${styles.text}`}>A E-Clinic disponibiliza instalações modernas para prestar atendimento de qualidade a seus pacientes. Humanização, cuidado, atenção e respeito são palavras que orientam há anos todas as atividades das equipes.</p>
         <p className={`mb-4 ${styles.text}`}>Centro cirúrgico completo, com 17 salas preparadas para realizar quaisquer tipos de procedimentos, dos mais simples aos de alta complexidade, incluindo cirurgias por videolaparoscopia, tornando os procedimentos menos incisivos e minimizando o período de internação.</p>
-        <Carousel indicators={true} nextIcon={<span aria-hidden="true" className="carousel-control-next-icon" />} prevIcon={<span aria-hidden="true" className="carousel-control-prev-icon" />}>
+      </div>
+          <Carousel indicators={true} nextIcon={<span aria-hidden="true" className="carousel-control-next-icon" />} prevIcon={<span aria-hidden="true" className="carousel-control-prev-icon" />}>
           {Array.from({ length: slides }).map((_, slideIndex) => (
             <Carousel.Item key={slideIndex}>
               <div className="d-flex justify-content-center align-items-center">
@@ -63,7 +64,6 @@ const Installations: React.FC = () => {
             </Carousel.Item>
           ))}
         </Carousel>
-      </div>
       <img src="/images/home/img-00.png" className={`${styles.waveStyle}`} alt="Instalação" />
     </div>
   );
