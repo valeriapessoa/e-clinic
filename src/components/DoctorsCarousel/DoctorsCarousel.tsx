860dbf7961ae015ef2a76b28cf354367cf6da288
Valéria Pessoa <valeriapessoa2005@gmail.com>
2024-12-27 01:06:27 -0300

    Atualização de layout para telas pequenas - convenios e unidades

--- src/components/DoctorsCarousel/DoctorsCarousel.tsx
+++ src/components/DoctorsCarousel/DoctorsCarousel.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 import { FaFacebookF, FaTwitter } from "react-icons/fa";
 import { BiLogoInstagramAlt } from "react-icons/bi";
 import TertiaryTitle from "../TertiaryTitle/TertiaryTitle";
-import styles from "./DoctorsCarousel.module.css";
 import { Carousel } from "react-bootstrap";
+import styles from "./DoctorsCarousel.module.css";
 
 interface CarouselItem {
   name: string;
@@ -21,34 +21,56 @@ interface DoctorsCarouselProps {
 }
 
 const DoctorsCarousel: React.FC<DoctorsCarouselProps> = ({ title, subtitle, items }) => {
+  const [itemsPerSlide, setItemsPerSlide] = useState(3);
+
+  useLayoutEffect(() => {
+    const updateItemsPerSlide = () => {
+      if (window.innerWidth < 768) {
+        setItemsPerSlide(1);
+      } else {
+        setItemsPerSlide(3);
+      }
+    };
+
+    // Chama a função no primeiro render
+    updateItemsPerSlide();
+
+    // Adiciona um listener para redimensionamento da janela
+    window.addEventListener("resize", updateItemsPerSlide);
+
+    // Limpa o listener ao desmontar o componente
+    return () => window.removeEventListener("resize", updateItemsPerSlide);
+  }, []);
+
+  // Calcular o número total de slides
+  const slides = Math.ceil(items.length / itemsPerSlide);
+
   return (
-    <div className={styles.carouselContainer}>
+    <div className={`${styles.carouselContainer}`}>
       <TertiaryTitle primary={title} secondary={subtitle} />
       <Carousel indicators={true} controls={true} interval={5000} className={`carousel doctors ${styles.customCarousel}`}>
-        {Array.from({ length: Math.ceil(items.length / 3) }).map((_, slideIndex) => (
+        {Array.from({ length: slides }).map((_, slideIndex) => (
           <Carousel.Item key={slideIndex}>
-            <div className="d-flex justify-content-center">
-              {items.slice(slideIndex * 3, slideIndex * 3 + 3).map((item, index) => (
-                <div key={index} className={`${styles.card} ${styles.customGap}`}>
-                  <div className={styles.imageContainer}>
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className={styles.itemImage}
-                    />
-                  </div>
-                  <h5 className={styles.itemName}>{item.name}</h5>
-                  <p className={styles.itemDescription}>{item.description}</p>
-                  <div className={styles.socialIcons}>
-                    <a href={item.instagramLink} target="_blank" rel="noopener noreferrer">
-                      <BiLogoInstagramAlt className={styles.icon} />
-                    </a>
-                    <a href={item.facebookLink} target="_blank" rel="noopener noreferrer">
-                      <FaFacebookF className={styles.icon} />
-                    </a>
-                    <a href={item.twitterLink} target="_blank" rel="noopener noreferrer">
-                      <FaTwitter className={styles.icon} />
-                    </a>
+            <div className="row justify-content-center">
+              {items.slice(slideIndex * itemsPerSlide, slideIndex * itemsPerSlide + itemsPerSlide).map((item, index) => (
+                <div key={index} className="col-12 col-sm-6 col-md-4">
+                  <div className="card text-center">
+                    <img src={item.image} alt={item.name} className="card-img-top" />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <p className="card-text">{item.description}</p>
+                      <div className="social-icons">
+                        <a href={item.instagramLink} target="_blank" rel="noopener noreferrer">
+                          <BiLogoInstagramAlt className="icon" size={24}/>
+                        </a>
+                        <a href={item.facebookLink} target="_blank" rel="noopener noreferrer">
+                          <FaFacebookF className="icon ms-3" size={24}/>
+                        </a>
+                        <a href={item.twitterLink} target="_blank" rel="noopener noreferrer">
+                          <FaTwitter className="icon ms-3" size={24}/>
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))}
