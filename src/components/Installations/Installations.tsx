f37eff7bfd66fde7980560a9088fb41c0dfda040
Valéria Pessoa <valeriapessoa2005@gmail.com>
2024-12-28 18:13:44 -0300

    Remover setas laterais do carrossel

--- src/components/Installations/Installations.tsx
+++ src/components/Installations/Installations.tsx
@@ -3,7 +3,7 @@ import { Carousel } from 'react-bootstrap';
 import SecondaryTitle from '../SecondaryTitle/SecondaryTitle';
 import styles from './Installations.module.css';
 
-const Installations: React.FC = () => {
+const Installations = () => {
   const [itemsPerSlide, setItemsPerSlide] = useState(3);
 
   useLayoutEffect(() => {
@@ -15,13 +15,10 @@ const Installations: React.FC = () => {
       }
     };
 
-    // Chama a função no primeiro render
     updateItemsPerSlide();
 
-    // Adiciona um listener para redimensionamento da janela
     window.addEventListener("resize", updateItemsPerSlide);
 
-    // Limpa o listener ao desmontar o componente
     return () => window.removeEventListener("resize", updateItemsPerSlide);
   }, []);
 
@@ -38,7 +35,6 @@ const Installations: React.FC = () => {
     { src: "/images/home/img-9.png", alt: "Instalação 9" },
   ];
 
-  // Calcular o número total de slides
   const slides = Math.ceil(items.length / itemsPerSlide);
 
   return (
@@ -53,17 +49,17 @@ const Installations: React.FC = () => {
         <p className={`mb-4 ${styles.text}`}>A E-Clinic disponibiliza instalações modernas para prestar atendimento de qualidade a seus pacientes. Humanização, cuidado, atenção e respeito são palavras que orientam há anos todas as atividades das equipes.</p>
         <p className={`mb-4 ${styles.text}`}>Centro cirúrgico completo, com 17 salas preparadas para realizar quaisquer tipos de procedimentos, dos mais simples aos de alta complexidade, incluindo cirurgias por videolaparoscopia, tornando os procedimentos menos incisivos e minimizando o período de internação.</p>
       </div>
-          <Carousel indicators={true} nextIcon={<span aria-hidden="true" className="carousel-control-next-icon" />} prevIcon={<span aria-hidden="true" className="carousel-control-prev-icon" />}>
-          {Array.from({ length: slides }).map((_, slideIndex) => (
-            <Carousel.Item key={slideIndex}>
-              <div className="d-flex justify-content-center align-items-center">
-                {items.slice(slideIndex * itemsPerSlide, slideIndex * itemsPerSlide + itemsPerSlide).map((item, index) => (
-                  <img key={index} src={item.src} className={`d-block ${styles.carouselImg}`} alt={item.alt} />
-                ))}
-              </div>
-            </Carousel.Item>
-          ))}
-        </Carousel>
+      <Carousel indicators={true} nextIcon={null} prevIcon={null}>
+        {Array.from({ length: slides }).map((_, slideIndex) => (
+          <Carousel.Item key={slideIndex}>
+            <div className="d-flex justify-content-center align-items-center">
+              {items.slice(slideIndex * itemsPerSlide, slideIndex * itemsPerSlide + itemsPerSlide).map((item, index) => (
+                <img key={index} src={item.src} className={`d-block ${styles.carouselImg}`} alt={item.alt} />
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
       <img src="/images/home/img-00.png" className={`${styles.waveStyle}`} alt="Instalação" />
     </div>
   );
